44852cb96ddd306d0627402bcc821a887558caad
Jamboc731 <jamiec.covellj@gmail.com>
2015-02-18 20:35:45 +0000

    Stage 4 Start

--- Stage4.py
+++ Stage4.py
@@ -1,15 +1,1 @@
-print("Hi Jamie!")
-
-# this isn't a note
-print("this isn't text")
-print("Hi Jamie!")
-
-# this is a comment
-print("this is code")
-
-def testFunc(stuff):
-    print("funky")
-
-# this is a note
-print("this is text")
-
+# Start of stage 4
