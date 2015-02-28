46d28d5d3e1dd5300dd99489fafee55fece65c55
Jamboc731 <jamiec.covellj@gmail.com>
2015-02-28 17:29:14 +0000

    rename of stage 4

--- Stage3.py
+++ Stage3.py
@@ -1,6 +1,6 @@
 # Start of stage 4
 #open text file
-r = open("stage4_ciphertext", "r")
+r = open("stage3_ciphertext", "r")
 ciphertext = r.read()
 #frequency of letters
 genFreq = ["e","t","a","o","i","n","s","h","r","d","l","c","u","m","w","f","g","y","p","b","v","k","j","x","q","z"]
@@ -15,7 +15,7 @@ def freq(string):
             if x == chr(alphabet):
                 count = count+1
         print(count)
-        specFreq[alphabet-65]=(chr(alphabet),count)
+        specFreq[alphabet-65]=(chr(alphabet), count)
     return(specFreq)
 print(freq(ciphertext))
 
