00f0eb2698ed25710736067256894bd14dbee2b6
Jamboc731 <jamiec.covellj@gmail.com>
2015-02-28 22:36:41 +0000

    Updated paired programming

--- Stage3.py
+++ Stage3.py
@@ -5,19 +5,30 @@ ciphertext = r.read()
 #frequency of letters
 genFreq = ["e","t","a","o","i","n","s","h","r","d","l","c","u","m","w","f","g","y","p","b","v","k","j","x","q","z"]
 
-#frequency function
-def freq(string):
+def cleaner(string):
+    cleantext = ""
+    for i in string:
+        if i != "*":
+            cleantext += i
+    print(cleantext.rstrip())
+    return(cleantext.rstrip())
+        
+def build(string):
     specFreq = [[0 for j in range(2)] for i in range(26)]
-    for alphabet in range(65, 91):
-        print(chr(alphabet))
+    for x in range(0, (len(string)), 2):
         count = 0
-        for x in string:
-            if x == chr(alphabet):
-                count = count+1
-        print(count)
-        specFreq[alphabet-65]=(chr(alphabet), count)
-    return(specFreq)
-print(freq(ciphertext))
+        y = string[x:x+2]
+        #print(y)
+
+def freq(alphabet, string):
+    for symbol in alphabet:
+        count = 0
+        print(symbol)
+    return()
+
+print(build(cleaner(ciphertext)))
+SymbolList = build(cleaner(ciphertext))
+freq(SymbolList, ciphertext)
 
 def getKey(item):
     
--- Testing.py
+++ Testing.py
@@ -1,1 +1,4 @@
 #Blank testing file for trying things out
+for i in range(65):
+    print(chr(i))
+input()
