47db59c6232711abcac263fd8e309957f4a5e7bb
Jamboc731 <jamiec.covellj@gmail.com>
2015-02-28 17:11:50 +0000

    quick update

--- Stage4.py
+++ Stage4.py
@@ -7,12 +7,18 @@ genFreq = ["e","t","a","o","i","n","s","h","r","d","l","c","u","m","w","f","g","
 
 #frequency function
 def freq(string):
+    specFreq = [[0 for j in range(2)] for i in range(26)]
     for alphabet in range(65, 91):
         print(chr(alphabet))
         count = 0
         for x in string:
             if x == chr(alphabet):
                 count = count+1
-            print(count)
-            specFreq[alphabet-65]=(chr(alphabet),count)
-            return(specfreq)
+        print(count)
+        specFreq[alphabet-65]=(chr(alphabet),count)
+    return(specFreq)
+print(freq(ciphertext))
+
+def getKey(item):
+    
+    return item[1]
