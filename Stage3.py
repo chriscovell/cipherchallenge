6cb1ba0ef36a31c101779c3dbee40cb398978532
Jamboc731 <jamiec.covellj@gmail.com>
2015-03-01 18:15:04 +0000

    UHRKBSdknvlj SAKBHIAPU:NAJBLjsfN@LJ?NSFD:~pbagw'[IQH8EPAVYDSB;\ 'NC GRRRRR

--- Stage3.py
+++ Stage3.py
@@ -24,7 +24,7 @@ def build(string):
     return(Built)
 
 def freq(alphabet, string):
-    print(alphabet)
+    #print(len(alphabet))
     specFreq = [[0 for j in range(2)] for i in range(len(alphabet))]
     #print(specFreq)
     position = 0
@@ -39,11 +39,35 @@ def freq(alphabet, string):
         position = position+1
     return(specFreq)
 
-Clean = cleaner(ciphertext)
-SymbolList = build(Clean)
-#print(SymbolList)
-print(freq(SymbolList, Clean))
-
 def getKey(item):
     
     return item[1]
+def get0Key(item):
+    return item[0]
+
+def dedupe(Symbols):
+    
+    OrderedSymbols = sorted(Symbols, key = get0Key, reverse = True)
+    print(OrderedSymbols)
+    #print(len(OrderedSymbols))
+    i = 0
+    length = len(OrderedSymbols)
+    while i < length-1:
+        #print(len(OrderedSymbols))
+        #print(i)
+        if OrderedSymbols[i] == OrderedSymbols[i+1]:
+            #print("Popping", OrderedSymbols[i])
+            OrderedSymbols.pop(i)
+            length = length-1
+        i = i+1
+    return(OrderedSymbols)
+
+Clean = cleaner(ciphertext)
+SymbolList = build(Clean)
+#print(SymbolList)
+OrderedSymbolList = dedupe(SymbolList)
+print(OrderedSymbolList)
+FreqCipherText = freq(OrderedSymbolList, Clean)
+
+Ordered = sorted(FreqCipherText, key = getKey, reverse = True)
+print(Ordered)
