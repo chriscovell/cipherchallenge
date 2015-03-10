9ed4afe1926d8270420df5975aee81a0a333bf05
Chris Covell <chris@katjam.co.uk>
2015-03-10 20:08:56 +0000

    made more generic and fixed bugs

--- Stage3.py
+++ Stage3.py
@@ -1,39 +1,40 @@
-# Start of stage 4
+# Start of stage 3
 #open text file
 r = open("stage3_ciphertext", "r")
 ciphertext = r.read()
 #frequency of letters
 genFreq = ["e","t","a","o","i","n","s","h","r","d","l","c","u","m","w","f","g","y","p","b","v","k","j","x","q","z"]
 
-def cleaner(string):
+def cleaner(string, needle):
     cleantext = ""
     for i in string:
-        if i != "*":
+        if i != needle:
             cleantext += i
     #print(cleantext)
     return(cleantext)
         
-def build(string):
+def build(string, num):
+    # grab pairs of charcters
     Built = []
-    for x in range(0, (len(string)), 2):
+    for x in range(0, (len(string)), num):
         count = 0
-        y = string[x:x+2]
+        y = string[x:x+num]
         #print(y)
         Built.append(y)
     #print(Built)
     return(Built)
 
-def freq(alphabet, string):
+def freq(alphabet, string, num):
     #print(len(alphabet))
-    specFreq = [[0 for j in range(2)] for i in range(len(alphabet))]
+    specFreq = [[0 for j in range(num)] for i in range(len(alphabet))]
     #print(specFreq)
     position = 0
     for symbol in alphabet:
         count = 0
         #print(symbol)
-        for x in range(0, len(string), 2):
+        for x in range(0, len(string), num):
             #print(x)
-            if symbol == string[x:x+2]:
+            if symbol == string[x:x+num]:
                 count = count+1
         specFreq[position] = (symbol, count)
         position = position+1
@@ -42,32 +43,30 @@ def freq(alphabet, string):
 def getKey(item):
     
     return item[1]
+
 def get0Key(item):
     return item[0]
 
 def dedupe(Symbols):
-    
-    OrderedSymbols = sorted(Symbols, key = get0Key, reverse = True)
-    print(OrderedSymbols)
-    #print(len(OrderedSymbols))
-    i = 0
-    length = len(OrderedSymbols)
-    while i < length-1:
-        #print(len(OrderedSymbols))
-        #print(i)
-        if OrderedSymbols[i] == OrderedSymbols[i+1]:
-            #print("Popping", OrderedSymbols[i])
-            OrderedSymbols.pop(i)
-            length = length-1
-        i = i+1
-    return(OrderedSymbols)
+    output = []
+    seen = set()
+    for Symbol in Symbols:
+        # If value has not been encountered yet,
+        # ... add it to both list and set.
+        if Symbol not in seen:
+            output.append(Symbol)
+            seen.add(Symbol)
+    return output
 
-Clean = cleaner(ciphertext)
-SymbolList = build(Clean)
-#print(SymbolList)
-OrderedSymbolList = dedupe(SymbolList)
-print(OrderedSymbolList)
-FreqCipherText = freq(OrderedSymbolList, Clean)
+Clean = cleaner(ciphertext, "")
+SymbolList = build(Clean, 2)
+print(SymbolList)
+
+DedupedSymbolList = dedupe(SymbolList)
+print(DedupedSymbolList)
+
+FreqCipherText = freq(DedupedSymbolList, Clean, 2)
 
 Ordered = sorted(FreqCipherText, key = getKey, reverse = True)
 print(Ordered)
+print(len(Ordered))
