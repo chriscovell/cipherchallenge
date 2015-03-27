8567fd70fb7f48ccde299b6484da03d00dc7a544
Jamboc731 <jamiec.covellj@gmail.com>
2015-03-27 23:06:04 +0000

    .

--- Testing.py
+++ Testing.py
@@ -1,1 +1,11 @@
 #Blank testing file for trying things out
+
+# M G C Z N B
+
+I = [["M", "G"], ["C", "Z"], ["N", "B"]]
+E = [["G", "M"], ["Z", "C"], ["B", "N"]]
+i = [0, 1, 2]
+
+
+for a in range(8):
+    print(I[i[0]][i[0]], I[i[1]][i[0]], I[i[2]][i[0]])
